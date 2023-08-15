eb3dc39edea7772e0fedbc85667ca9aeee4b3cd2
Juan Pablo <99679469+JotaPeEsse@users.noreply.github.com>
2023-08-15 12:02:20 -0300

    update

--- features/Iniciar_el_juego.feature
+++ features/Iniciar_el_juego.feature
@@ -8,18 +8,29 @@ Scenario: Iniciar el juego
     When hago clic en el botón "Jugar"
     Then debería ser redirigido a la página del juego
 
-
-Scenario: Ganar el juego
-    Given que estoy en la página de juego
-    When adivino correctamente todas las letras de la palabra oculta
-    Then el dibujo del muñeco del ahorcado debería estar en su estado inicial
-    Then el número de intentos restantes debería mantenerse
-    Then debería ver el mensaje "¡Ganaste!"
-    Then cierro el navegador
-    
 Scenario: Adivinar una letra correcta
-    Given estoy en la página de juego
+    Given que estoy en la página de juego
     When ingreso la letra "a"
     Then debería ver la letra "a" en la palabra oculta
-    Then el dibujo del muñeco del ahorcado debería seguir vacío
-    Then el número de intentos restantes debe mantenerse  
+    And el dibujo del muñeco del ahorcado debería seguir vacío
+    And el número de intentos restantes debería mantenerse
+
+Scenario: Adivinar una letra incorrecta
+    Given el contador de intentos está en 6
+    When ingreso letra "z"
+    Then debería ver el dibujo del muñeco del ahorcado con una parte dibujada
+    And el contador de intentos disminuye en 1
+
+Scenario: Ganar el juego
+    Given estoy en la página de juego
+    When adivino correctamente todas las letras de la palabra oculta
+    Then el dibujo del muñeco del ahorcado debería estar en su estado inicial
+    Then el número de intentos restantes debe mantenerse
+    Then debería ver el mensaje "¡Ganaste!"
+
+Scenario: Perder el juego
+    Given que estoy en la página del juego 
+    When adivino incorrectamente todas las letras de la palabra oculta
+    Then debería ver el dibujo completo del muñeco del ahorcado
+    And el número de intentos restantes debería llegar a cero
+    And debería ver el mensaje "¡Perdiste!"
--- features/steps/iniciar_juego.py
+++ features/steps/iniciar_juego.py
@@ -1,60 +1,117 @@
-from playwright.sync_api import Page, Browser, sync_playwright
-from behave import given, when, then
-from bs4 import BeautifulSoup
-
-import time
-
-BASE_URL = 'https://martinb.pythonanywhere.com/juego'
-
+from playwright.async_api import async_playwright
 from playwright.sync_api import sync_playwright
 
-def iniciar_navegador():
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        return browser, page
+import asyncio
+from bs4 import BeautifulSoup
+from behave import given, when, then
+import time
+
+# Definir el URL base para la página de juego
+BASE_URL = 'https://martinb.pythonanywhere.com/juego's
 
 
+# ---------------------------------------Iniciar Juego-------------------------------------------------------------
+@given('que estoy en la página de inicio')
+async def step_estoy_en_pagina_inicio(context):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context.browser = browser
+        context.page = await browser.new_page()
+        await context.page.goto('https://martinb.pythonanywhere.com/')
+        time.sleep(2)
 
-"-----------------------------------------------------Iniciar-Juego-----------------------------------------------------"  
+@when('hago clic en el botón "{button_text}"')
+async def step_hago_clic_en_jugar(context, button_text):
+    await context.page.goto('https://martinb.pythonanywhere.com/')
+    await context.page.wait_for_selector(f'button:has-text("{button_text}")')
+    button = await context.page.query_selector(f'button:has-text("{button_text}")')
+    await button.click()
+    time.sleep(2)
 
-def ingresar_letra(page: Page, letra: str):
+@then('debería ser redirigido a la página del juego')
+async def step_deberia_ser_redirigido(context):
+    target_url = 'https://martinb.pythonanywhere.com/juego'
+    current_url = context.page.url
+    assert current_url == target_url, f"URL actual: {current_url}, URL objetivo: {target_url}"
+    time.sleep(2)
+
+# ---------------------------------------Adivinar una Letra Correcta-------------------------------------------------------------
+
+@given('que estoy en la página de juego')
+async def step_abrir_pagina_de_juego(context):
+    
+    browser = await async_playwright().start().chromium.launch(headless=False)
+    context.browser = browser
+    context.page = await browser.new_page()
+    await context.page.goto(BASE_URL)
+
+@when('ingreso la letra "{letra}"')
+async def step_ingreso_letra(context, letra):
+    await context.page.fill('input[name="letra"]', letra)
+    await context.page.click('button[type="submit"]')
+
+@then('debería ver la letra "{letra}" en la palabra oculta')
+async def step_ver_letra_en_palabra_oculta(context, letra):
+    palabra_oculta = await context.page.inner_text('#pal h2')
+    assert letra in palabra_oculta
+
+@then('el dibujo del muñeco del ahorcado debería seguir vacío')
+async def step_deberia_ver_dibujo_vacio(context):
+    page_source = await context.page.content()
+    soup = BeautifulSoup(page_source, 'html.parser')
+    dibujo_muñeco = soup.find('div', id='dibujo').pre.text
+
+    assert '+---+' in dibujo_muñeco
+    assert 'O   |' not in dibujo_muñeco
+
+@then('el número de intentos restantes debería mantenerse')
+async def step_ver_intentos_restantes(context):
+    intentos_restantes = await context.page.inner_text('#intentos h2')
+    assert intentos_restantes == 'Intentos restantes: 6'
+
+# ---------------------------------------Adivinar una Letra Incorrecta-------------------------------------------------------------
+
+@given('el contador de intentos está en 6')
+async def step_contador_intentos_inicial(context):
+    browser = await async_playwright().start().chromium.launch(headless=False)
+    context.browser = browser
+    context.page = await browser.new_page()
+    await context.page.goto(BASE_URL)
+    intentos_restantes = await context.page.inner_text('#intentos h2')
+    assert intentos_restantes == 'Intentos restantes: 6'
+
+@when('ingreso letra "{letra}"')
+async def step_ingreso_letra_incorrecta(context, letra):
+    await context.page.fill('input[name="letra"]', letra)
+    await context.page.click('button[type="submit"]')
+
+@then('debería ver el dibujo del muñeco del ahorcado con una parte dibujada')
+async def step_ver_dibujo_con_parte_dibujada(context):
+    page_source = await context.page.content()
+    soup = BeautifulSoup(page_source, 'html.parser')
+    dibujo_muñeco = soup.find('div', id='dibujo').pre.text
+
+    assert '+---+' in dibujo_muñeco
+    assert 'O   |' in dibujo_muñeco
+    assert '/|\\' not in dibujo_muñeco
+
+@then('el contador de intentos disminuye en 1')
+async def step_contador_intentos_disminuye(context):
+    intentos_restantes = await context.page.inner_text('#intentos h2')
+    assert intentos_restantes == 'Intentos restantes: 5'
+
+# --------------------------------------- Ganar el Juego -------------------------------------------------------------
+
+
+# Función para ingresar una letra en el campo de entrada
+def ingresar_letra(page, letra):
     input_selector = 'input[name="letra"]'
     page.wait_for_selector(input_selector)
     page.fill(input_selector, letra)
     page.click('button[type="submit"]')
 
-@given('que estoy en la página de inicio')
-def step_estoy_en_pagina_inicio(context):
-    with sync_playwright() as p:
-        browser: Browser = p.chromium.launch(headless=False)
-        context.browser = browser
-        context.page = browser.new_page()
-        context.page.goto('https://martinb.pythonanywhere.com/')
-        time.sleep(2)
-
-@when('hago clic en el botón "{button_text}"')
-def step_hago_clic_en_jugar(context, button_text):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context.page = browser.new_page()
-        context.page.goto('https://martinb.pythonanywhere.com/')
-        context.page.wait_for_selector(f'button:has-text("{button_text}")')
-        button = context.page.query_selector(f'button:has-text("{button_text}")')
-        button.click()
-        time.sleep(2)
-        
-        
-@then('debería ser redirigido a la página del juego')
-def step_deberia_ser_redirigido(context):
-    target_url =  'https://martinb.pythonanywhere.com/juego'
-    current_url = context.page.url
-    assert current_url == target_url, f"URL actual: {current_url}, URL objetivo: {target_url}"
-    time.sleep(2)
-
-"-----------------------------------------------------Ganar-Juego-----------------------------------------------------"  
-
-@given('que estoy en la página de juego')
+# Steps para el escenario de adivinar correctamente todas las letras de la palabra oculta
+@given('estoy en la página de juego')
 def step_abrir_pagina_de_juego(context):
     context.browser = sync_playwright().start().chromium.launch(headless=False)
     context.page = context.browser.new_page()
@@ -69,8 +126,8 @@ def step_adivino_correctamente(context):
 
     # Crear un conjunto para almacenar las letras adivinadas
     letras_adivinadas = set()
-    
-     # Obtener el número de intentos restantes antes de adivinar las letras
+
+    # Obtener el número de intentos restantes antes de adivinar las letras
     intentos_restantes_text = context.page.inner_text('#intentos h2')
     intentos_restantes = int(intentos_restantes_text.split()[-1])
 
@@ -78,84 +135,89 @@ def step_adivino_correctamente(context):
     for letra in palabra_oculta:
         if letra not in letras_adivinadas:
             ingresar_letra(context.page, letra)
-            time.sleep(2)
+            time.sleep(2)  # Agrega una pausa entre las adivinanzas para evitar problemas
             letras_adivinadas.add(letra)
-    # Actualizar la palabra oculta después de cada adivinanza
-        palabra_oculta_actualizada = context.page.inner_text('#pal h2').strip().lower()
-        letras_ocultas = set(palabra_oculta_actualizada)
 
     # Imprimir la palabra oculta final después de adivinar todas las letras
+    palabra_oculta_actualizada = context.page.inner_text('#pal h2').strip().lower()
     print(f'Palabra oculta final: {palabra_oculta_actualizada}')
-    time.sleep(1)
 
 @then('el dibujo del muñeco del ahorcado debería estar en su estado inicial')
 def step_ver_dibujo_estado_inicial(context):
- 
     page_source = context.page.content()
     soup = BeautifulSoup(page_source, 'html.parser')
     dibujo_muñeco = soup.find('div', id='dibujo').pre.text
 
     assert '+---+' in dibujo_muñeco
     assert 'O   |' not in dibujo_muñeco
-    time.sleep(1)
+    time.sleep(2)
 
-@then('el número de intentos restantes debería mantenerse')
+@then('el número de intentos restantes debe mantenerse')
 def step_ver_intentos_restantes(context):
-    context.page.wait_for_selector('#intentos h2')
     intentos_restantes_text = context.page.inner_text('#intentos h2')
     assert 'Intentos restantes' in intentos_restantes_text
     assert intentos_restantes_text.split()[-1] != ''  # Aseguramos que el valor no esté vacío
     assert 'Intentos restantes: 6' in intentos_restantes_text
-    time.sleep(1)
-
-
+    time.sleep(2)
 
 @then('debería ver el mensaje "¡Ganaste!"')
 def step_verificar_mensaje_ganador(context):
     mensaje_ganador_element = context.page.wait_for_selector('#resultado h2')
     mensaje_ganador = mensaje_ganador_element.inner_text()
     assert '¡Ganaste!' in mensaje_ganador
-    time.sleep(2)    
     
-@then('cierro el navegador')
-def step_cerrar_navegador(context):
-    context.browser.close() 
+    
 
-"--------------------------------Adivinar-Letra---------------------------------------------------------------"
+# --------------------------------------- Perder el Juego -------------------------------------------------------------
 
+# Función para ingresar una letra incorrecta en el campo de entrada
+def ingresar_letra_incorrecta(page, letra):
+    input_selector = 'input[name="letra"]'
+    page.wait_for_selector(input_selector)
+    page.fill(input_selector, letra)
+    page.click('button[type="submit"]')
 
-
-@given('estoy en la página de juego')
-def step_estoy_en_pagina_inicio(context):
-    with sync_playwright() as p:
-        browser: Browser = p.chromium.launch(headless=False)
+@given('que estoy en la página del juego')
+async def step_abrir_pagina_del_juego(context):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
         context.browser = browser
-        context.page = browser.new_page()
-        context.page.goto('https://martinb.pythonanywhere.com/juego')
+        context.page = await browser.new_page()
+        await context.page.goto(BASE_URL)
         time.sleep(2)
 
-@when('ingreso la letra "{letra}"')
-def step_ingreso_letra(context, letra):
-    context.page.fill('input[name="letra"]', letra)
-    context.page.click('button[type="submit"]')
-    time.sleep(1)
+@when('adivino incorrectamente todas las letras de la palabra oculta')
+async def step_adivino_incorrectamente(context):
+    await step_abrir_pagina_del_juego(context)
+    
+    # Obtener la palabra oculta de la página
+    palabra_oculta = await context.page.inner_text('#palabra h2').strip().lower()
 
-@then('debería ver la letra "{letra}" en la palabra oculta')
-def step_ver_letra_en_palabra_oculta(context, letra):
-    palabra_oculta = context.page.inner_text('#pal h2')
-    assert letra in palabra_oculta
-    time.sleep(1)
-@then('el dibujo del muñeco del ahorcado debería seguir vacío')
-def step_deberia_ver_dibujo_vacio(context):
-    page_source = context.page.content()
-    soup = BeautifulSoup(page_source, 'html.parser')
-    dibujo_muñeco = soup.find('div', id='dibujo').pre.text
+    # Lista de letras incorrectas
+    letras_incorrectas = ['x'] * 6
+    
+    for letra in letras_incorrectas:
+        await asyncio.sleep(2)
+        await ingresar_letra_incorrecta(context.page, letra)
+    time.sleep(2)
 
-    assert '+---+' in dibujo_muñeco
-    assert 'O   |' not in dibujo_muñeco
-    time.sleep(1)
-@then('el número de intentos restantes debe mantenerse')
-def step_ver_intentos_restantes(context):
-    intentos_restantes = context.page.inner_text('#intentos h2')
-    assert intentos_restantes == 'Intentos restantes: 6'
-    time.sleep(1)
+@then('debería ver el dibujo completo del muñeco del ahorcado')
+async def step_verificar_dibujo_completo(context):
+    page_source = await context.page.content()
+    assert '+---+' in page_source
+    assert 'O   |' in page_source
+    assert '/|\\  |' in page_source
+    assert '/ \\  |' in page_source
+
+@then('el número de intentos restantes debería llegar a cero')
+async def step_verificar_numero_intentos_cero(context):
+    intentos_restantes_text = await context.page.inner_text('#intentos h2')
+    assert 'Intentos restantes: 0' in intentos_restantes_text
+    time.sleep(2)
+
+@then('debería ver el mensaje "¡Perdiste!"')
+async def step_verificar_mensaje_perdiste(context):
+    mensaje_perdiste_element = await context.page.wait_for_selector('#resultado h2')
+    mensaje_perdiste = await mensaje_perdiste_element.inner_text()
+    assert '¡Perdiste!' in mensaje_perdiste
+    time.sleep(2)
